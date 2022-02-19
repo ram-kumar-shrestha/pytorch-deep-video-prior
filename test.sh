24b94976d3a52b85cac35eb00b4e3e5383336743
Rujengelal <34258344+Rujengelal@users.noreply.github.com>
2022-02-19 10:10:47 +0545

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python main_IRT.py --max_epoch 25 --input demo/colorization/goat_input --processed demo/colorization/goat_processed --model colorization --with_IRT 1 --IRT_initialization 1 --output ./
+python main_IRT.py --max_epoch 5 --input demo/colorization/goat_input --processed demo/colorization/goat_processed --model colorization --with_IRT 1 --IRT_initialization 1 
